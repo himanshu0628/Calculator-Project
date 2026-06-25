cc9a7226ebe97cdaafc8452f5b51402f61df726e
himanshu0628 <himanshuu0628@gmail.com>
2026-06-25 11:34:12 +0530

    Create menu

---
+++ Scientific-calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    double a,b;
+    printf("Scientific calculator\n");
+    printf("1 - Addition\n");
+    printf("2 - Subtraction\n");
+    printf("3 - Multiplication\n");
+    printf("4 - Division\n");
+    printf("5 - Power\n");
+    printf("6 - Square route\n");
+    printf("7 - Sine\n");
+    printf("8 - Cosine\n");
+    printf("9 - Tangent\n");
+    printf("10 - Logarithm\n");
+    printf("11 - Natural log\n");
+}
